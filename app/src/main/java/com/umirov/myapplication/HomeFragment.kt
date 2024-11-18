241d60fa96b1fa5b27d0bc7fe9481a6ea6d35533
umiro <umirovtimur5@gmail.com>
2024-11-18 21:46:01 +0900

    search bar behavior

--- app/src/main/java/com/umirov/myapplication/HomeFragment.kt
+++ app/src/main/java/com/umirov/myapplication/HomeFragment.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.umirov.myapplication.databinding.FragmentHomeBinding
 
 
 class HomeFragment : Fragment() {
+
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding
     private lateinit var filmsAdapter: FilmListRecyclerAdapter
@@ -66,15 +68,30 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding?.searchView?.setOnClickListener {
+            binding?.searchView?.isIconified = false
 
+        }
 
+        binding?.searchView?.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return true
+            }
 
+            override fun onQueryTextChange(newText: String?): Boolean {
+                if (newText.isNullOrEmpty()) {
+                    filmsAdapter.addItems(filmsDataBase)
+                    return true
+                }
+                val result = filmsDataBase.filter {
+                    it.title.contains(newText.toString(), ignoreCase = true)
 
+                }
+                filmsAdapter.addItems(result)
+                return true
 
-
-
-
-
+            }
+        })
 
 
 
